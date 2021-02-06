48d31b3bcd25e1041f2619bf5cd609e7737898f6
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-02-06 03:23:26 +0300

    feat: wrote ciphering machine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,80 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    constructor(isDirect = true){
+        this.direct = isDirect
+    }
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    encrypt(message , key) {
+        if(!message || !key){
+            throw new Error('Missing argument!')
+        }
+      let plainTextArray = message.toUpperCase().split('')
+      let keyArray = []
+      let result = []
+      let keyArrayNativeLength = key.length
+      let k = key.length
+      while(keyArray.length !== plainTextArray.length){
+        let keyIndex = k % keyArrayNativeLength
+        if(this.alphabet.indexOf(plainTextArray[keyArray.length]) !== -1){
+            keyArray.push(key[keyIndex].toUpperCase())
+            k++
+        }
+        else {
+            keyArray.push(plainTextArray[keyArray.length])
+        }
+      }
+      for(let i = 0; i < plainTextArray.length; i++){
+          if(this.alphabet.split('').indexOf(plainTextArray[i]) !== -1){
+            let resultIndex = this.alphabet.indexOf(plainTextArray[i]) + this.alphabet.indexOf(keyArray[i])
+            if(resultIndex < 26){
+                result.push(this.alphabet[resultIndex])
+            }
+            else{
+                result.push(this.alphabet[resultIndex-26])
+            }
+          }
+          else{
+              result.push(plainTextArray[i])
+          }
+      }
+      return this.direct ? result.join('') : result.reverse().join('')
+    }    
+    decrypt(encryptedMessage , key) {
+        if(!encryptedMessage || !key){
+            throw new Error('Missing argument!')
+        }
+        let plainTextArray = encryptedMessage.toUpperCase().split('')
+        let keyArray = []
+        let result = []
+        let keyArrayNativeLength = key.length
+        let k = key.length
+        while(keyArray.length !== plainTextArray.length){
+          let keyIndex = k % keyArrayNativeLength
+          if(this.alphabet.indexOf(plainTextArray[keyArray.length]) !== -1){
+              keyArray.push(key[keyIndex].toUpperCase())
+              k++
+          }
+          else {
+              keyArray.push(plainTextArray[keyArray.length])
+          }
+        }
+        for(let i = 0; i < plainTextArray.length; i++){
+            if(this.alphabet.split('').indexOf(plainTextArray[i]) !== -1){
+              let resultIndex = this.alphabet.indexOf(plainTextArray[i]) - this.alphabet.indexOf(keyArray[i])
+              if(resultIndex >= 0){
+                  result.push(this.alphabet[resultIndex])
+              }
+              else{
+                  result.push(this.alphabet[26 + resultIndex])
+              }
+            }
+            else{
+                result.push(plainTextArray[i])
+            }
+        }
+        return this.direct ? result.join('') : result.reverse().join('')
+    }
   }
-}
 
 module.exports = VigenereCipheringMachine;
